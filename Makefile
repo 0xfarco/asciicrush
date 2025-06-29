77c9644b6ca5c59712363a418c34f518e571b8b6
0xfarco <0xfarco@gmail.com>
2025-06-29 21:33:14 +0530

    main game loop

---
+++ Makefile
@@ -0,0 +1,14 @@
+CC = g++
+CFLAGS = -Iinclude/
+LDFLAGS = -Llib/ -l:libraylib.a -lm
+SRC = main.c
+OUT = main
+
+all: $(OUT)
+
+$(OUT): $(SRC)
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+clean:
+	rm -f $(OUT)
+
--- main.c
+++ main.c
@@ -1,3 +1,45 @@
+#include <raylib.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+
+#define BOARD_SIZE 8
+#define TILE_SIZE 42
+#define TILE_TYPES 5
+
+const char tile_chars[TILE_TYPES] = { '#', '@', '$', '%', '&' };
+char board[BOARD_SIZE][BOARD_SIZE];
+
+char random_tile() {
+    return tile_chars[rand() % TILE_TYPES];
+}
+
+void init_board() {
+    for (int y = 0; y < BOARD_SIZE; y++) {
+        for (int x = 0; x < BOARD_SIZE; x++) {
+            board[y][x] = random_tile();
+        }
+    }
+}
+
 int main() {
+    const int screen_width = 800;
+    const int screen_height = 450;
+
+    InitWindow(screen_width, screen_height, "ASCII Crush - A 2D Game");
+    SetTargetFPS(60);
+    srand(time(NULL));
+
+    init_board();
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
