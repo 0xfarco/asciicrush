f38ce83548e52f638ce14e753ac8093089e80f2a
0xfarco <0xfarco@gmail.com>
2025-06-30 06:21:10 +0530

    centering tile board

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 const char tile_chars[TILE_TYPES] = { '#', '@', '$', '%', '&' };
 char board[BOARD_SIZE][BOARD_SIZE];
 
+Vector2 grid_origin;
+
 char random_tile() {
     return tile_chars[rand() % TILE_TYPES];
 }
@@ -21,6 +23,14 @@ void init_board() {
             board[y][x] = random_tile();
         }
     }
+
+    int grid_width = BOARD_SIZE * TILE_SIZE;
+    int grid_height = BOARD_SIZE * TILE_SIZE;
+
+    grid_origin = (Vector2) {
+        (GetScreenWidth() - grid_width) / 2,
+        (GetScreenHeight() - grid_height) / 2
+    };
 }
 
 int main() {
@@ -40,8 +50,8 @@ int main() {
         for (int y = 0; y < BOARD_SIZE; y++) {
             for (int x = 0; x < BOARD_SIZE; x++) {
                 Rectangle rect = {
-                    x * TILE_SIZE,
-                    y * TILE_SIZE,
+                    grid_origin.x + (x * TILE_SIZE),
+                    grid_origin.y + (y * TILE_SIZE),
                     TILE_SIZE,
                     TILE_SIZE
                 };
