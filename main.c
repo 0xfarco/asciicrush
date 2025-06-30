e8e1cbf6c5deb62cd6dfe8540ff48230bf14a582
0xfarco <0xfarco@gmail.com>
2025-06-30 06:09:42 +0530

    drawing tile board

--- main.c
+++ main.c
@@ -36,6 +36,29 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                Rectangle rect = {
+                    x * TILE_SIZE,
+                    y * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE
+                };
+
+                DrawRectangleLinesEx(rect, 1, DARKGRAY);
+
+                DrawTextEx(
+                    GetFontDefault(),
+                    TextFormat("%c", board[y][x]),
+                    (Vector2) {
+                        rect.x + 12, rect.y + 8
+                    },
+                    20, 1, WHITE
+                );
+            }
+        }
+
         EndDrawing();
     }
 
